40fe84a0d9b5b7e3c6224112f149a912eb275f07
Kienfpt202 <94580960+Kienfpt202@users.noreply.github.com>
2022-12-28 17:58:29 +0700

    remove failed codes

--- Login/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Login.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
